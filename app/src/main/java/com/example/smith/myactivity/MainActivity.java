3b1df190b3db88115b838eb8fb0f94594127e808
androidYZP <18626312413@163.com>
2016-12-26 15:24:36 +0800

    activity的全面分析

---
+++ app/src/main/java/com/example/smith/myactivity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.smith.myactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    /**
+     * 生命周期的第一个方法,可以做一些初始化工作
+     * 加载界面的布局资源,初始化Activity所需数据
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * 表示正在重新启动,一般情况下,当
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
